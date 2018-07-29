518f146bcf04344cc105c6c855f8fa9667f4d428
Patrick Connelly <pconnell@redhat.com>
2018-07-29 16:19:41 -0400

    Adding tests for statics

---
+++ tests/lib/statics.test.js
@@ -0,0 +1,14 @@
+var statics = require('../../src/lib/statics.js');
+
+test('Check log types', function () {
+    var expectedResults = {
+        CONNECTION: {
+            SANDBOX_URL: 'https://test.salesforce.com',
+            PROD_URL: 'https://login.salesforce.com',
+            VERSION: '43.0'
+        },
+        DATETIME_FORMAT: 'YYYY-MM-DDTHH:mm:ss.000\\Z',
+        DATE_FORMAT: 'YYYY-MM-DD'
+    };
+    expect(statics).toEqual(expect.objectContaining(expectedResults));
+});
